cf36e9a485d8853a472ae9528f515351ba98a21c
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-28 23:33:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,8 @@ class Song
   
   def self.find_or_create_by_name(name)
     binding.pry
-    if !name.find_by_name
-      name.create_by_name
+    if !self.find_by_name(name)
+      self.create_by_name(name)
     end
   end
 end
